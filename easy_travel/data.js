ce3320e34465289491bf835234523878f613dbea
pzumaetab <85259780+pzumaetab@users.noreply.github.com>
2023-04-11 05:11:21 -0500

    Add files via upload

---
+++ easy_travel/data.js
@@ -0,0 +1,23 @@
+module.exports = function () {
+  var data = {
+    Empresa_Transporte: [
+      {
+        idEmpresa:1,
+        nombre: "Movil Bus",
+      },
+      {
+        idEmpresa:2,
+        nombre: "Ittsa Bus",
+      },
+      {
+        idEmpresa:3,
+        nombre: "Excluciva",
+      },
+      {
+        idEmpresa:4,
+        nombre: "Civa",
+	    }
+    ],
+  }
+  return data
+}
